08137bc4eed6ef5b9ec1cde9446481c834a77911
Sebastian Kootz <skxxtz@gmail.com>
2025-06-18 00:43:07 +0200

    feat: new pomodoro timer implemented

--- src/ui/tiles/pomodoro_tile.rs
+++ src/ui/tiles/pomodoro_tile.rs
@@ -1,19 +1,6 @@
-use crate::{
-    actions::commandlaunch::command_launch,
-    daemon::daemon::SizedMessage,
-    g_subclasses::sherlock_row::SherlockRow,
-    launcher::{pomodoro_launcher::Pomodoro, Launcher},
-    sherlock_error,
-    utils::errors::{SherlockError, SherlockErrorType},
-};
+use crate::{actions::commandlaunch::command_launch, daemon::daemon::SizedMessage, g_subclasses::sherlock_row::SherlockRow, launcher::{pomodoro_launcher::Pomodoro, Launcher}, sherlock_error, utils::errors::{SherlockError, SherlockErrorType}};
+use std::{cell::{Cell, RefCell}, io::Write, path::PathBuf, rc::Rc, time::{Duration, SystemTime, UNIX_EPOCH}, usize};
 use std::os::unix::net::UnixStream;
-use std::{
-    cell::RefCell,
-    io::Write,
-    path::PathBuf,
-    rc::Rc,
-    time::{Duration, SystemTime, UNIX_EPOCH},
-};
 
 use super::Tile;
 
@@ -25,14 +12,11 @@ impl Tile {
         object.append(&tile);
         object.set_css_classes(&vec!["tile", "timer-tile"]);
         object.with_launcher(launcher);
-
+        
         if let Some(title) = &launcher.name {
             imp.timer_title.set_text(title);
         }
-        let pomodoro_api = Rc::new(RefCell::new(PomodoroInterface::new(
-            pomodoro,
-            imp.remaining_label.downgrade(),
-        )));
+        let pomodoro_api = Rc::new(RefCell::new(PomodoroInterface::new(pomodoro, imp.remaining_label.downgrade(), imp.animation.downgrade())));
         pomodoro_api.borrow_mut().update_ui();
         // initialize view
         // gather result from pomodoro tile
@@ -48,21 +32,25 @@ impl Tile {
     }
 }
 
-struct PomodoroInterface {
+struct PomodoroInterface{
     socket: PathBuf,
     exec: PathBuf,
     update_field: WeakRef<Label>,
+    animation: WeakRef<Picture>,
     handle: Option<SourceId>,
-    running: bool,
+    running: Rc<Cell<bool>>,
+    frames: Rc<Vec<Pixbuf>>,
 }
-impl PomodoroInterface {
-    fn new(pomodoro: &Pomodoro, label: WeakRef<Label>) -> Self {
+impl PomodoroInterface{
+    fn new(pomodoro: &Pomodoro, label: WeakRef<Label>, anim: WeakRef<Picture>)->Self{
         Self {
             socket: pomodoro.socket.clone(),
             exec: pomodoro.program.clone(),
             update_field: label,
+            animation: anim,
             handle: None,
-            running: false,
+            running: Rc::new(Cell::new(false)),
+            frames: Rc::new(Self::get_animation().unwrap_or_default()),
         }
     }
     fn send_message(&self, message: &str) -> Result<(), SherlockError> {
@@ -80,8 +68,34 @@ impl PomodoroInterface {
         })?;
         Ok(())
     }
-    fn toggle(&mut self) {
-        if self.running {
+    fn get_animation()->Option<Vec<Pixbuf>>{
+        let animation = gdk_pixbuf::PixbufAnimation::from_resource("/dev/skxxtz/sherlock/ui/timer.gif").ok()?;
+        let mut frames: Vec<Pixbuf> = vec![];
+        let mut start_time = SystemTime::now();
+        let iter = animation.iter(Some(start_time));
+        loop {
+            match iter.delay_time(){
+                Some(delay) => {
+                    start_time = start_time.checked_add(delay).unwrap();
+                },
+                _ => {
+                    break;
+                }
+            };
+
+            if let Some(buf) = iter.pixbuf().copy() {
+                frames.push(buf);
+            }
+
+            let advanced = iter.advance(start_time);
+            if !advanced {
+                break;
+            }
+        }
+        Some(frames)
+    }
+    fn toggle(&mut self){
+        if self.running.get() {
             self.stop();
         } else {
             self.start();
@@ -90,9 +104,7 @@ impl PomodoroInterface {
     }
     fn start(&mut self) {
         match self.send_message("start") {
-            Ok(_) => {
-                self.running = true;
-            }
+            Ok(_) => {},
             Err(e) if matches!(e.error, SherlockErrorType::SocketConnectError(_)) => {
                 // start pomodoro service
                 let exec = self.exec.display().to_string();
@@ -103,7 +115,7 @@ impl PomodoroInterface {
                 if let Err(e) = self.send_message("start") {
                     let _result = e.insert(false);
                 } else {
-                    self.running = true;
+                    self.running.set(true);
                 }
             }
             Err(e) => {
@@ -111,14 +123,18 @@ impl PomodoroInterface {
             }
         }
     }
-    fn stop(&mut self) {
-        if let Err(e) = self.send_message("stop") {
+    fn stop(&mut self){
+        if !self.running.get() {
+            return
+        }
+
+        if let Err(e) = self.send_message("stop"){
             let _result = e.insert(false);
         }
         if let Some(handle) = self.handle.take() {
             handle.remove();
         }
-        self.running = false;
+        self.running.set(false);
     }
     fn get_timer(&self) -> Option<Timer> {
         let mut stream = UnixStream::connect(&self.socket).ok()?;
@@ -130,13 +146,20 @@ impl PomodoroInterface {
         }
         None
     }
-    fn update_ui(&mut self) {
-        if let Some(handle) = self.handle.take() {
-            handle.remove();
-        }
+    fn update_ui(&mut self){
         if let Some(timer) = self.get_timer() {
-            self.running = timer.active;
+            if self.running.get() {
+                if let Some(handle) = self.handle.take() {
+                    handle.remove();
+                }
+            }
+            self.running.set(timer.active);
             let label = self.update_field.clone();
+            let animation = self.animation.clone();
+            let length = self.frames.len() as u64;
+            let frames = Rc::clone(&self.frames);
+            let mut remaining = timer.remaining().as_secs();
+            let mut current_frame = length - length * remaining / 1500;
             let update_label = move |time: u64| {
                 if let Some(label) = label.upgrade() {
                     let mins = time / 60;
@@ -144,20 +167,53 @@ impl PomodoroInterface {
                     label.set_text(&format!("{:0>2}:{:0>2}", mins, secs));
                 }
             };
-            let mut remaining = timer.remaining().as_secs();
+            let update_anim = move || {
+                if let Some(image) = animation.upgrade() {
+                    if let Some(pix) = frames.get(current_frame as usize){
+                        let paintable = Texture::for_pixbuf(pix);
+                        image.set_paintable(Some(&paintable));
+                    }
+                }
+            };
             update_label(remaining);
+            update_anim();
             if timer.active {
-                let handle = glib::timeout_add_local(Duration::new(1, 0), {
+                let handle = glib::timeout_add_local(Duration::new(1,0), {
+                    let is_running = Rc::clone(&self.running);
                     move || {
                         while remaining > 0 {
                             remaining -= 1;
+                            let new_frame = length - length * remaining / 1500;
+                            if current_frame != new_frame {
+                                current_frame = new_frame;
+                                update_anim();
+                            }
                             update_label(remaining);
                             return true.into();
                         }
+                        is_running.set(false);
                         false.into()
                     }
                 });
                 self.handle = Some(handle);
+            }
+        } else {
+            self.update_label(1500);
+            self.update_anim(0);
+        }
+    }
+    fn update_label(&self, time: u64){
+        if let Some(label) = self.update_field.upgrade() {
+            let mins = time / 60;
+            let secs = time % 60;
+            label.set_text(&format!("{:0>2}:{:0>2}", mins, secs));
+        }
+    }
+    fn update_anim(&self, frame: usize){
+        if let Some(image) = self.animation.upgrade() {
+            if let Some(pix) = self.frames.get(frame){
+                let paintable = Texture::for_pixbuf(pix);
+                image.set_paintable(Some(&paintable));
             }
         }
     }
@@ -203,7 +259,7 @@ mod imp {
     use std::cell::RefCell;
 
     use gio::glib::{SignalHandlerId, SourceId, WeakRef};
-    use gtk4::glib;
+    use gtk4::{glib, Picture};
     use gtk4::subclass::prelude::*;
     use gtk4::CompositeTemplate;
     use gtk4::{Box as GtkBox, Label};
@@ -218,6 +274,9 @@ mod imp {
 
         #[template_child(id = "remaining_time")]
         pub remaining_label: TemplateChild<Label>,
+
+        #[template_child(id = "animation")]
+        pub animation: TemplateChild<Picture>,
 
         pub return_action: RefCell<Option<SignalHandlerId>>,
         pub time_out_handle: RefCell<Option<SourceId>>,
@@ -244,13 +303,9 @@ mod imp {
     impl BoxImpl for TimerTile {}
 }
 
-use gdk_pixbuf::subclass::prelude::ObjectSubclassIsExt;
+use gdk_pixbuf::{prelude::{PixbufAnimationExtManual}, subclass::prelude::ObjectSubclassIsExt, Pixbuf};
 use gio::glib::{object::ObjectExt, SourceId, WeakRef};
-use gtk4::{
-    glib,
-    prelude::{BoxExt, WidgetExt},
-    Label,
-};
+use gtk4::{gdk::Texture, glib, prelude::{BoxExt, WidgetExt}, Label, Picture};
 use serde::Deserialize;
 
 glib::wrapper! {
