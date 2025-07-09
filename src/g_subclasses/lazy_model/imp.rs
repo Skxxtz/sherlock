fb6730cd30e4649e130c85cc9495fc947b2331a9
Sebastian Kootz <skxxtz@gmail.com>
2025-07-09 17:04:31 +0200

    impl: foundational sherlock lazy box for effficient ui updates

--- src/g_subclasses/lazy_model/imp.rs
+++ src/g_subclasses/lazy_model/imp.rs
@@ -1,1 +1,36 @@
+use gio::glib::subclass::Signal;
+use gtk4::prelude::*;
+use gtk4::subclass::prelude::*;
+use gtk4::glib;
+use std::sync::OnceLock;
 
+
+#[derive(Default)]
+pub struct SherlockLazyBox { }
+
+// The central trait for subclassing a GObject
+#[glib::object_subclass]
+impl ObjectSubclass for SherlockLazyBox {
+    const NAME: &'static str = "CustomSherlockRow";
+    type Type = super::SherlockLazyBox;
+    type ParentType = gtk4::Box;
+}
+
+// Trait shared by all GObjects
+impl ObjectImpl for SherlockLazyBox {
+    fn constructed(&self) {
+        self.parent_constructed();
+    }
+    fn signals() -> &'static [glib::subclass::Signal] {
+        static SIGNALS: OnceLock<Vec<Signal>> = OnceLock::new();
+        SIGNALS.get_or_init(|| {
+            vec![Signal::builder("row-should-activate")
+                .param_types([u8::static_type(), String::static_type()])
+                .build()]
+        })
+    }
+}
+
+// Make SherlockRow function with `IsA widget and ListBoxRow`
+impl WidgetImpl for SherlockLazyBox {}
+impl BoxImpl for SherlockLazyBox {}
--- src/g_subclasses/lazy_model/mod.rs
+++ src/g_subclasses/lazy_model/mod.rs
@@ -1,1 +1,27 @@
 mod imp;
+
+use glib::Object;
+use gtk4::{
+    glib,
+    prelude::WidgetExt,
+};
+
+glib::wrapper! {
+    pub struct SherlockLazyBox(ObjectSubclass<imp::SherlockLazyBox>)
+        @extends gtk4::Box, gtk4::Widget;
+}
+
+impl SherlockLazyBox {
+    pub fn new() -> Self {
+        let myself: Self = Object::builder().build();
+        myself.add_css_class("tile");
+        myself
+    }
+}
+
+impl Default for SherlockLazyBox {
+    fn default() -> Self {
+        let row = Self::new();
+        row
+    }
+}
