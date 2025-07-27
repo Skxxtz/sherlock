245fc003f6bf1701a3f6cc03ee4af0e689cf3ff5
kimu <kimukamii@gmail.com>
2025-07-27 19:34:40 +0200

    fix: Exec command wasn't split on quoted arguments

--- src/actions/applaunch.rs
+++ src/actions/applaunch.rs
@@ -20,13 +20,8 @@ pub fn applaunch(exec: &str, terminal: bool) -> Option<()> {
         parts.push(flag.to_string());
     }
 
-    let cmd = parts.join(" ").replace('"', "");
-
-    let mut parts = cmd
-        .trim()
-        .split_whitespace()
-        .filter(|s| !s.starts_with("%"));
-
+    let cmd = parts.join(" ").trim().to_string();
+    let mut parts = split_as_command(&cmd).into_iter();
     let mut command = Command::new(parts.next()?);
     command.args(parts);
 
@@ -45,4 +40,48 @@ pub fn applaunch(exec: &str, terminal: bool) -> Option<()> {
         .spawn()
         .map_err(|e| eprintln!("Error executing command: {}", e));
     None
+}
+
+pub fn split_as_command(cmd: &str) -> Vec<String> {
+    let mut parts = Vec::new();
+    let mut current = String::new();
+    let mut quoting = false;
+    let mut prev = '\0';
+    let mut double_escape = false;
+
+    for c in cmd.chars() {
+        if double_escape {
+            // Escape inside quotes in Exec value as specified by
+            // https://specifications.freedesktop.org/desktop-entry-spec/latest/exec-variables.html
+            double_escape = false;
+            match c {
+                '"' | '`' | '$' | '\\' => {
+                    current.pop();
+                    current.push(c);
+                    prev = '\0';
+                    continue;
+                }
+                _ => current.push('\\')
+            }
+        }
+        if quoting && c == '\\' && prev == '\\' {
+            double_escape = true;
+        } else if c == '"' {
+            quoting = !quoting;
+        } else if !quoting && c.is_whitespace() && !current.is_empty() {
+            parts.push(current.clone());
+            current.clear();
+        } else {
+            current.push(c);
+        }
+        prev = c;
+    }
+
+    if !current.is_empty() {
+        parts.push(current);
+    }
+
+    parts.retain(|s| !s.starts_with("%"));
+
+    parts
 }
